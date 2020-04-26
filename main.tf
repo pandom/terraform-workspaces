1e87d55c8d4bb62a6dfdd75751007b771ccf6640
Grant Orchard <go@hashicorp.com>
2020-04-26 20:41:55 +1000

    update module source

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "github" {
 }
 
 module "terraform-aws-awx" {
-  source = source  = "app.terraform.io/grantorchard/workspace/tfe"
+  source = "app.terraform.io/grantorchard/workspace/tfe"
   repository_name = "terraform-aws-awx"
   oauth_token_id = var.oauth_token_id
   env_var = var.aws_env_var
 }
 
 module "terraform-aws-core" {
-  source = source  = "app.terraform.io/grantorchard/workspace/tfe"
+  source = "app.terraform.io/grantorchard/workspace/tfe"
   repository_name = "terraform-aws-core"
   oauth_token_id = var.oauth_token_id
   env_var = var.aws_env_var
