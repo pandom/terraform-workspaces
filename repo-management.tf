fbd0d7f21ebc3c5be0d6df9cff5cb9473ef95b0d
Anthony Burke <pandom@users.noreply.github.com>
2020-10-16 14:43:42 +1100

    Changes

--- repo-management.tf
+++ repo-management.tf
@@ -1,8 +1,8 @@
-module "unmanaged-github-repos" {
-  source = "app.terraform.io/grantorchard/workspace/tfe"
-  providers = {
-    github = github.personal
-  }
-  repository_name = "unmanaged-github-repos"
-  oauth_token_id = var.oauth_token_id
-}
+// module "unmanaged-github-repos" {
+//   source = "app.terraform.io/grantorchard/workspace/tfe"
+//   providers = {
+//     github = github.personal
+//   }
+//   repository_name = "unmanaged-github-repos"
+//   oauth_token_id = var.oauth_token_id
+// }
