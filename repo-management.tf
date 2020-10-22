96c7ef56cfa2b24bff882051df5b8c79f679d61d
Anthony Burke <pandom@users.noreply.github.com>
2020-10-22 15:57:24 +1100

    Adding packer and deployment repos

--- repo-management.tf
+++ repo-management.tf
@@ -1,8 +1,40 @@
-// module "unmanaged-github-repos" {
-//   source = "app.terraform.io/grantorchard/workspace/tfe"
-//   providers = {
-//     github = github.personal
-//   }
-//   repository_name = "unmanaged-github-repos"
-//   oauth_token_id = var.oauth_token_id
-// }
+module "tf-packer-consul" {
+  source = "app.terraform.io/burkey/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "tf-packer-consul"
+  oauth_token_id = var.oauth_token_id
+}
+module "tf-packer-nomad" {
+  source = "app.terraform.io/burkey/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "tf-packer-nomad"
+  oauth_token_id = var.oauth_token_id
+}
+module "tf-packer-vault" {
+  source = "app.terraform.io/burkey/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "tf-packer-vault"
+  oauth_token_id = var.oauth_token_id
+}
+module "tf-packer-waypoint" {
+  source = "app.terraform.io/burkey/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "tf-packer-waypoint"
+  oauth_token_id = var.oauth_token_id
+}
+module "tf-packer-boundary" {
+  source = "app.terraform.io/burkey/workspace/tfe"
+  providers = {
+    github = github.personal
+  }
+  repository_name = "tf-packer-boundary"
+  oauth_token_id = var.oauth_token_id
+}
