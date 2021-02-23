85b6fb93e3bc7728fd507339e383c092a1707311
pandom <pandom@users.noreply.github.com>
2021-02-23 13:21:49 +1100

    Adds Crown Repo

--- main.tf
+++ main.tf
@@ -21,6 +21,24 @@ locals {
 }
 
 
+module "tf-azure-crown" {
+  source = "app.terraform.io/burkey/workspace/tfe"
+  version = "0.0.20" 
+  providers = {
+    github = github.personal
+  }
+  repository_name = "tf-azure-crown"
+  create_repo = true
+  repository_private = false
+  oauth_token_id = var.oauth_token_id
+  tf_var = merge({
+    "ssh_public_key" = {
+      "value" = local.ssh_public_key,
+      "sensitive" = false
+    }
+  },
+  var.slack_webhook)
+}
 
 
 module "tf-azure-bigdata" {
