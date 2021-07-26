b88861b7056f7e91d07c0cbcc933075574ed0961
Anthony Burke <burkey@Anthonys-iMac.local>
2021-07-26 10:49:26 +1000

    reset

--- main.tf
+++ main.tf
@@ -24,26 +24,26 @@ locals {
 }
 ## Terraform Token Lab
 
-module "tf-devstack" {
-  source = "app.terraform.io/burkey/workspace/tfe"
-  version = "0.0.24" 
-  providers = {
-    github = github.personal
-  }
-  repository_name = "tf-devstack"
-  create_repo = true
-  repository_private = false
-  oauth_token_id = var.oauth_token_id
-  execution_mode = "agent"
-  agent_pool_id = data.tfe_agent_pool.laptop_agent_pool.id
-  tf_var = merge({
-    "ssh_public_key" = {
-      "value" = local.ssh_public_key,
-      "sensitive" = false
-    }
-  },
-  var.slack_webhook)
-}
+# module "tf-devstack" {
+#   source = "app.terraform.io/burkey/workspace/tfe"
+#   version = "0.0.24" 
+#   providers = {
+#     github = github.personal
+#   }
+#   repository_name = "tf-devstack"
+#   create_repo = true
+#   repository_private = false
+#   oauth_token_id = var.oauth_token_id
+#   execution_mode = "agent"
+#   agent_pool_id = data.tfe_agent_pool.laptop_agent_pool.id
+#   tf_var = merge({
+#     "ssh_public_key" = {
+#       "value" = local.ssh_public_key,
+#       "sensitive" = false
+#     }
+#   },
+#   var.slack_webhook)
+# }
 
 
 module "tf-nomad-ipam" {
